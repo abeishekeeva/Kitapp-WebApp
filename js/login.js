63c335c2cb2786cc8b5744d28b840190255867a0
aidai beishekeeva <aidai.beishekeeva@gmail.com>
2018-02-12 00:50:05 +0600

    12.02.2018 with the new design

---
+++ js/login.js
@@ -0,0 +1,10 @@
+
+  function showLoginForm() {
+    $('#register_form').hide();
+    $('#login_form').show();
+  }
+
+  function showRegisterForm() {
+    $('#login_form').hide();
+    $('#register_form').show();
+  }
